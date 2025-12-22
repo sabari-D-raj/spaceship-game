6f1d2b9ca0bfca8470c7e9900e3a9dd50131d66b
sabari-D-raj <shabariraj9@gmail.com>
2025-12-22 19:09:34 +0530

    third commit

--- spacegame.py
+++ spacegame.py
@@ -10,9 +10,32 @@ background= pygame.image.load("images/Parallax60.png").convert()
 background=pygame.transform.scale(background,(width,height))
 player= pygame.image.load("images/rockect.png").convert_alpha()
 player=pygame.transform.scale(player,(100,100))
+asteriod_img=pygame.image.load("images/asteriod.png").convert_alpha()
+asteriod_img=pygame.transform.scale(asteriod_img,(200,200))
 player_x,player_y=350,690
 bg_visible,bg_above,bg_speed =0,-height,3
 
+def create_rock():
+    return {
+        "x":random.randint(0,width-200),
+        "y": random.randint(-1200,-200),
+        "speed":random.randint(4,7)
+    }
+def reeset_asteriod(a):
+    asteriod["x"] = random.randint(0, width - 200)
+    asteriod["y"] = random.randint(-1200, -200)
+    asteriod["speed"] = random.randint(4, 8)
+def bullet_fire():
+    bulletx=player_x+100  //2-bullet_width//2
+    bullety=player_y
+    rect=pygame.Rect(bulletx,bullety,bullet_width,bullet_height)
+    bullet.append(rect)
+asteriods=[]
+bullet=[]
+bullet_width,bullet_height= 8,16
+bullet_speed=12
+for i in range(5):
+    asteriods.append(create_rock())
 running=True
 while running:
     fps.tick(60)
@@ -30,11 +53,25 @@ while running:
         player_x+=5
     if key[pygame.K_a]:
         player_x-=5
+    if key[pygame.K_SPACE]:
+        bullet_fire() 
     if player_x>=800 or player_x<=0:
-        print("out")     
+        print("out")
+    for asteriod in asteriods:
+        asteriod["y"] += asteriod["speed"]
+        if asteriod["y"] > height:
+            reeset_asteriod(asteriod)
+
+    if asteriod["y"]>=height:
+            asteriod["x"]=random.randint(0,width-200)
+            asteriod["y"]=random.randint(-1200,-200)
+            asteriod["speed"]=random.randint(4,7)
     screen.blit(background,(0,bg_visible))
     screen.blit(background,(0,bg_above))
     screen.blit(player,(player_x,player_y))
+    for asteriod in asteriods:
+        screen.blit(asteriod_img,(asteriod["x"],asteriod["y"]))
+    
     pygame.display.update()
 pygame.quit()
 sys.exit()
